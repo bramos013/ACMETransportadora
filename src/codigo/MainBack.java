92ac1b7bbcfa998c6f706bcde3b3d0b926877907
bramos013 <brunoramos013@gmail.com>
2021-06-29 16:07:49 -0300

    Remoçao Parametros inutilizados

--- src/codigo/MainBack.java
+++ src/codigo/MainBack.java
@@ -3,10 +3,6 @@ package codigo;
 
 public class MainBack {
 
-    private Atendente atendente;
-
-    private Cliente cliente;
-
     public static void main(String[] args) {
         Atendente usuario = new Atendente();
         usuario.inicializaMenu();

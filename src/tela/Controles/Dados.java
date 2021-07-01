0deba21a5af5f67babdeeb456cd095314d53065a
André <lrandre601@gmail.com>
2021-06-30 21:49:22 -0300

    Arquivo executavel Acme.bat

--- src/tela/Controles/Dados.java
+++ src/tela/Controles/Dados.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dados {
-
     public static List<Cliente> listaClientes = new ArrayList<>();
     public static List<ClientePF> listaClientesPF = new ArrayList<>();
     public static List<ClientePJ> listaClientesPJ = new ArrayList<>();
     public static List<Aeroporto> listaAeroportos = new ArrayList<>();
     public static List<Carga> listaCargas = new ArrayList<>();
-
 
 }
